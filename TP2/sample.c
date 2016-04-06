878343d75165dbaf596f7f4685c000adbb1e8f5e
zipper97412 <zipper97412@gmail.com>
2016-04-06 13:21:55 +0200

    sdfqdsfq

---
+++ TP2/sample.c
@@ -0,0 +1,20 @@
+#include "ArbreBin.h"
+
+struct Client * createSampleTree() {
+  Client* arbre = createNode(15,0,0);
+  arbre->fg = createNode(12,0,0);
+  arbre->fg->fg = createNode(8,0,0);
+  arbre->fg->fg->fd = createNode(10,0,0);
+  arbre->fg->fd = createNode(14,0,0);
+  arbre->fg->fg->fg = createNode(13,0,0);
+  arbre->fd = createNode(20,0,0);
+  arbre->fd->fd = createNode(21,0,0);
+  arbre->fd->fg = createNode(16,0,0);
+  arbre->fg->fg->fd = createNode(17,0,0);
+  return arbre;
+}
+
+int main() {
+  Client* arbre = createSampleTree()
+  return 0;
+}
