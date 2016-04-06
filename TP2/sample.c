22aa18a2301c1028ef3b4ab564686763ccf1134e
zipper <zipper97412@gmail.com>
2016-04-06 11:31:46 +0000

    svsdfvds

--- TP2/sample.c
+++ TP2/sample.c
@@ -6,11 +6,11 @@ struct Client * createSampleTree() {
   arbre->fg->fg = createNode(8,0,0);
   arbre->fg->fg->fd = createNode(10,0,0);
   arbre->fg->fd = createNode(14,0,0);
-  arbre->fg->fg->fg = createNode(13,0,0);
+  arbre->fg->fd->fg = createNode(13,0,0);
   arbre->fd = createNode(20,0,0);
   arbre->fd->fd = createNode(21,0,0);
   arbre->fd->fg = createNode(16,0,0);
-  arbre->fg->fg->fd = createNode(17,0,0);
+  arbre->fd->fg->fd = createNode(17,0,0);
   return arbre;
 }
 
