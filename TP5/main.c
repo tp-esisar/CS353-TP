759793c3612bcb560bb8b8203e138b8e30bc64af
zipper97412 <zipper97412@gmail.com>
2016-05-10 14:20:20 +0200

    osef

--- TP5/main.c
+++ TP5/main.c
@@ -7,34 +7,45 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fullTableRandom(unsigned int seed) {
-	int randCodes[TABLE_SIZE];
+void insert_Random(int nb, unsigned int seed) {
 	char str[32];
-	int i,j,k,found;
+	int i=0,k;
 	srand(seed);
-	for(i = 0;i<TABLE_SIZE;i++) {
+	while(i<nb) {
 		k = rand()%100000;
-		//printf("%d\t", k);
-		found = 0;
-		for(j = 0;j<i;j++) {
-			if(randCodes[j] == k) {
-				found = 1;
-				break;
-			}
-		}
-		if(!found) {
-			
-			sprintf(str, "itemRand no %d", i);
+		if(searchItem(k) == -1) {
+			sprintf(str, "itemRand code: %d", k);
 			insertItem(k,str,k/100.0);
-			randCodes[i] = k;
-		}
-		else {
-			i--;
+			i++;
 		}
 	}
-	
 }
 
+void delete_Random(int nb, unsigned int seed) {
+	int i=0,k;
+	srand(seed);
+	while(i<nb) {
+		k = rand()%TABLE_SIZE;
+		if(hash_table[k].code != NULL_ITEM && hash_table[k].code != DELETED_ITEM) {
+			suppressItem(hash_table[i].code);
+			i++;
+		}
+	}
+}
+
+
+void afficheMeta() {
+	int indice;
+	printf("CODE\tPRIX\tINDEX\tDIRTY\n");
+	for (indice=0; indice<TABLE_SIZE; indice++) {
+		if (indice == NULL_ITEM)
+			printf("NULL\t%0.2f\t%d\t%s\n", hash_table[indice].price, indice, hash_table[indice].dirty==true?"true":"false");
+		else if (indice == DELETED_ITEM)
+			printf("DELL\t%0.2f\t%d\t%s\n", hash_table[indice].price, indice, hash_table[indice].dirty==true?"true":"false");
+		else
+			printf("%d\t%0.2f\t%d\t%s\n", hash_table[indice].code, hash_table[indice].price, indice, hash_table[indice].dirty==true?"true":"false");
+	}
+}
 
 int main()
 {
@@ -43,7 +54,7 @@ int main()
 
     // 
     init();
-	fullTableRandom(time(NULL));
+		insert_Random(TABLE_SIZE,time(NULL));
 /*    //
     int r;
     r = insertItem(10001,"Sucre",1.20f);
@@ -60,11 +71,41 @@ int main()
 
 */    //
     dumpItems();
-
+		
 
     //
     printf("=======Gestionnaire de magasin ======\n");
-
+		printf("=======Suppression aléatoire de la moitié des éléments ======\n");
+		delete_Random(TABLE_SIZE/2,time(NULL));
+		dumpItems();
+		printf("=======Ré-insertion des éléments ======\n");
+		insert_Random(TABLE_SIZE/2,time(NULL)+1);
+		dumpItems();
+		printf("=======Suppression de tous les éléments ======\n");
+		delete_Random(TABLE_SIZE,time(NULL)+2);
+		printf("=======Ré-insertion de la moitié des éléments avec seed 42 ======\n");
+		insert_Random(TABLE_SIZE/2,time(NULL));
+		dumpItems();
+		int item = 4242;
+		insertItem(item,"item à mette à jour",9999);
+		printf("*******Utilisation de getPrice pour l'item %d ******\n",item);
+		printf("Le Prix de l'item %d est %0.2f\n",item, getPrice(item));
+		printf("Mise à jour de cet item via updateItem(), Code de retour:%d\n",updateItem(item, "item mis à jour", 42.4242));
+		printf("Le Prix de l'item %d est %0.2f, son nom est \"%s\"\n",item, getPrice(item),hash_table[2].name);
+		dumpItems();
+		printf("*******Affichage des métadonnées de la table*****\n");
+		afficheMeta();
+		printf("*******Reconstruction de la table inSitu*********\n");
+		rebuildTable();
+		afficheMeta();
+		dumpItems();
+		printf("*******Test de findItem(), ajout d'un deuxième \"item mis à jour\" et recherche de ces deux items\n");
+		insertItem(314,"item mis à jour",3.14);
+		Item* found = findItem("item mis à jour");
+		printf("CODE\tLIBELLE\t\t\t\tPRIX\n");
+		printf("%d\t%s\t%0.2f\n", found[0].code, found[0].name, found[0].price);
+		printf("%d\t%s\t%0.2f\n", found[1].code, found[1].name, found[1].price);
+		
     return 0;
 }
 
--- TP6/src/rainbow/Main.java
+++ TP6/src/rainbow/Main.java
@@ -14,18 +14,20 @@ public class Main {
 		HashTable table = new HashTable(10061);
 		
 		System.out.println("ChaineCalcul(1) = "+chaine.CalculChaine(0, 1));			
-		
+		int nbitems=0;
 		for (int i=1; i<=10000; i++) {
 			int px = rand.nextInt(1000000);
 			int p999 = chaine.CalculChaine(0, px);
 
-			table.insert(p999, px);
+			if (table.insert(p999, px))
+				nbitems++;
 				
 			if (i%100 == 0)
 				System.out.print("\rAvancement : "+i/100+"%");
 		}
+		System.out.println("\n"+" elements dans la table\n");
 			
-		System.out.print("\nCréation du fichier... ");
+		System.out.print("\nCrï¿½ation du fichier... ");
 		File file = new File("rainbow.table");
 		ObjectOutputStream ecriture;
 		try {
@@ -33,7 +35,7 @@ public class Main {
 			ecriture.writeObject(table);
 	        ecriture.close();
 		} catch (IOException e) {
-			System.out.println("Erreur lors de l'écriture du fichier");
+			System.out.println("Erreur lors de l'ï¿½criture du fichier");
 			e.printStackTrace();
 		}
 		System.out.println("\nFini ! ");
