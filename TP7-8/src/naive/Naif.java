bce3a953313889c45f216394f3bd6a8e225e042a
zipper97412 <zipper97412@gmail.com>
2016-05-24 11:30:52 +0200

    dsqfqsdf

--- TP7-8/src/naive/Naif.java
+++ TP7-8/src/naive/Naif.java
@@ -3,19 +3,17 @@ package naive;
 public class Naif {
 	
 	int tab[][][][];
-	int m,n,i,j;
 	public Naif (int m, int n, int i, int j) {
-		this.m = m;
-		this.n = n;
-		this.i = i;
-		this.j = j;
-		tab = new int[m+1][n+1][i+1][j+1]; 
+		int dim = Math.max(m, n);
+		int dim2 = Math.max(i, j);
+		tab = new int[dim+1][dim+1][dim2+1][dim2+1]; 
 		for (int a=0; a<m+1; a++)
 			for (int b=0; b<n+1; b++)
 				for (int c=0; c<i+1; c++)
 					for (int d=0; d<j+1; d++)
 						tab[a][b][c][d]=-1;
 	}
+	public Naif(Pateau)
 	
 	
 	public int f_rec(int m,int n,int i,int j) {
@@ -93,7 +91,7 @@ public class Naif {
 	
 	public int f_dyn(int m,int n,int i,int j) {
 		//System.out.println(m+", "+ n+", "+i+", "+ j);
-		
+
 		if (tab[m][n][i][j] != -1)
 			return tab[m][n][i][j];
 		
@@ -102,18 +100,18 @@ public class Naif {
 		int maxSucc;
 		if(m>=2){
 			if(1<=i) {
-				maxSucc = tab[m-1][n][i-1][j] = f_dyn(m-1,n,i-1,j);
+				maxSucc = f_dyn(m-1,n,i-1,j);
 			}
 			else {
-				maxSucc = tab[1][n][i][j] = f_dyn(1,n,i,j);
+				maxSucc = f_dyn(1,n,i,j);
 			}
 		}
 		else {
 			if(1<=j) {
-				maxSucc = tab[m][n-1][i][j-1] = f_dyn(m,n-1,i,j-1);
+				maxSucc = f_dyn(m,n-1,i,j-1);
 			}
 			else {
-				maxSucc = tab[m][1][i][j] = f_dyn(m,1,i,j);
+				maxSucc = f_dyn(m,1,i,j);
 			}
 		}
 		int maxSuccNegatif=42;
@@ -122,10 +120,10 @@ public class Naif {
 		if(m>=2){
 			for(int k=1;k<m;k++) {
 				if(k<=i) {
-					temp = tab[m-k][n][i-k][j] = f_dyn(m-k,n,i-k,j);
+					temp = f_dyn(m-k,n,i-k,j);
 				}
 				else {
-					temp = tab[k][n][i][j] = f_dyn(k,n,i,j);
+					temp = f_dyn(k,n,i,j);
 				}
 				maxSucc = Math.max(temp, maxSucc);
 				if(temp<=0 && allSuccStrictPositif) {
@@ -142,10 +140,10 @@ public class Naif {
 		if(n>=2){
 			for(int k=1;k<n;k++) {
 				if(k<=j) {
-					temp = tab[m][n-k][i][j-k] = f_dyn(m,n-k,i,j-k);
+					temp = f_dyn(m,n-k,i,j-k);
 				}
 				else {
-					temp = tab[m][k][i][j] = f_dyn(m,k,i,j);
+					temp = f_dyn(m,k,i,j);
 				}
 				maxSucc = Math.max(temp, maxSucc);
 				if(temp<=0 && allSuccStrictPositif) {
@@ -159,11 +157,107 @@ public class Naif {
 				}
 			}
 		}
+		int result;
 		if(allSuccStrictPositif) {
-			return -(1+maxSucc);
+			result = -(1+maxSucc);
 		}
 		else {
-			return 1 - maxSuccNegatif;
+			result = 1 - maxSuccNegatif;
 		}
+		tab[m][n][i][j] = result;
+		return result;
 	}
+	
+	private void cacheWriteAccel(int m,int n,int i,int j,int value) {
+		tab[m][n][i][j] = value;
+		tab[m][n][m-i-1][j] = value;
+		tab[m][n][i][n-j-1] = value;
+		tab[m][n][m-i-1][n-j-1] = value;
+		tab[n][m][j][i] = value;
+		tab[n][m][n-j-1][i] = value;
+		tab[n][m][j][m-i-1] = value;
+		tab[n][m][n-j-1][m-i-1] = value;
+	}
+	
+	public int f_accel(int m,int n,int i,int j) {
+		//System.out.println(m+", "+ n+", "+i+", "+ j);
+		
+		if (tab[m][n][i][j] != -1)
+			return tab[m][n][i][j];
+		
+		if (m==1 && n==1) return 0;
+		
+		int maxSucc;
+		if(m>=2){
+			if(1<=i) {
+				maxSucc = f_accel(m-1,n,i-1,j);
+			}
+			else {
+				maxSucc = f_accel(1,n,i,j);
+			}
+		}
+		else {
+			if(1<=j) {
+				maxSucc = f_accel(m,n-1,i,j-1);
+			}
+			else {
+				maxSucc = f_accel(m,1,i,j);
+			}
+		}
+		int maxSuccNegatif=42;
+		boolean allSuccStrictPositif = true;
+		int temp;
+		if(m>=2){
+			for(int k=1;k<m;k++) {
+				if(k<=i) {
+					temp = f_accel(m-k,n,i-k,j);
+				}
+				else {
+					temp = f_accel(k,n,i,j);
+				}
+				maxSucc = Math.max(temp, maxSucc);
+				if(temp<=0 && allSuccStrictPositif) {
+					allSuccStrictPositif = false;
+					maxSuccNegatif = temp;
+				}
+				if(!allSuccStrictPositif) {
+					if(temp <= 0) {
+						maxSuccNegatif = Math.max(temp,maxSuccNegatif);
+					}
+				}
+			}
+		}
+		if(n>=2){
+			for(int k=1;k<n;k++) {
+				if(k<=j) {
+					temp = f_accel(m,n-k,i,j-k);
+				}
+				else {
+					temp = f_accel(m,k,i,j);
+				}
+				maxSucc = Math.max(temp, maxSucc);
+				if(temp<=0 && allSuccStrictPositif) {
+					allSuccStrictPositif = false;
+					maxSuccNegatif = temp;
+				}
+				if(!allSuccStrictPositif) {
+					if(temp <= 0) {
+						maxSuccNegatif = Math.max(temp,maxSuccNegatif);
+					}
+				}
+			}
+		}
+		int result;
+		if(allSuccStrictPositif) {
+			result = -(1+maxSucc);
+		}
+		else {
+			result = 1 - maxSuccNegatif;
+		}
+		cacheWriteAccel(m,n,i,j,result);
+		return result;
+	}
+	
 }
+
+
--- TP7-8/src/naive/test.java
+++ TP7-8/src/naive/test.java
@@ -3,17 +3,17 @@ package naive;
 public class test {
 
 	public static void main(String[] args) throws InterruptedException {
-		/**Récursif**/
+		/**RÃ©cursif**/
 		int m = 10; int n = 7; int i = 5; int j = 3;
-		Naif naif = new Naif(m,n,i,j);
-		
+//		Naif naif = new Naif(m,n,i,j);
+//		
 		long debut = System.currentTimeMillis();
-		//System.out.println(naif.f_rec(m,n,i,j));
-		long step = System.currentTimeMillis();
-		//System.out.println(naif.f_dyn(m,n,i,j));
+//		//System.out.println(naif.f_rec(m,n,i,j));
+//		long step = System.currentTimeMillis();
+//		//System.out.println(naif.f_dyn(m,n,i,j));
 		long fin = System.currentTimeMillis();
-		System.out.println("Temps (secondes) : \n- Récursif : " + (step-debut)/1000 + "\n- Dynamique : " + (fin-step)/1000 ); 
-		
+//		System.out.println("Temps (secondes) : \n- RÃ©cursif : " + (step-debut)/1000 + "\n- Dynamique : " + (fin-step)/1000 ); 
+//		
 		/**Dynamique**/
 		m = 100; n = 100; i = 48; j = 52;
 		Naif naif2 = new Naif(m,n,i,j);
@@ -23,19 +23,28 @@ public class test {
 		fin = System.currentTimeMillis();
 		System.out.println("Temps (secondes) : \n- Dynamique : " + (fin-debut)/1000 );
 		
-		/**Suite**/
-		m = 127; n = 127;
-		Naif naif3 = new Naif(m,n,m-1,n-1);
+		/**Accel**/
+		m = 100; n = 100; i = 48; j = 52;
+		Naif naif3 = new Naif(m,n,i,j);
 		
 		debut = System.currentTimeMillis();
-		for (i=0; i<m; i++)
-			for(j=0; j<n; j++)
-				if (naif3.f_dyn(m,n,i,j) == 127)
-					System.out.println("("+i+","+j+")");
+		System.out.println(naif3.f_accel(m,n,i,j));
 		fin = System.currentTimeMillis();
-		System.out.println("Temps (secondes) : \n- Dynamique : " + (fin-debut)/1000 );
+		System.out.println("Temps (secondes) : \n- Dynamique Accel : " + (fin-debut)/1000 );
 		
-		
+//		/**Suite**/
+//		m = 127; n = 127;
+//		Naif naif3 = new Naif(m,n,m-1,n-1);
+//		
+//		debut = System.currentTimeMillis();
+//		for (i=0; i<m; i++)
+//			for(j=0; j<n; j++)
+//				if (naif3.f_dyn(m,n,i,j) == 127)
+//					System.out.println("("+i+","+j+")");
+//		fin = System.currentTimeMillis();
+//		System.out.println("Temps (secondes) : \n- Dynamique : " + (fin-debut)/1000 );
+//		
+//		
 		
 	}
 
