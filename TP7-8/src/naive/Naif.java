de5d107118eb41327a3573068873ea94c7a8498d
zipper97412 <zipper97412@gmail.com>
2016-05-25 11:50:24 +0200

    fuck

--- TP7-8/src/naive/Naif.java
+++ TP7-8/src/naive/Naif.java
@@ -262,93 +262,66 @@ public class Naif {
 	}
 	
 	public int perfectPlay(Plateau p) {
-		int m = p.m;
-		int n = p.n;
-		int i = p.i;
-		int j = p.j;
 		
-		if (tab[m][n][i][j] != -1)
-			return tab[m][n][i][j];
-		
-		if (m==1 && n==1) return 0;
-		
+		if (p.m==1 && p.n==1) return 0;
+		Plateau bestPlay;
 		int maxSucc;
-		if(m>=2){
-			if(1<=i) {
-				maxSucc = f_accel(m-1,n,i-1,j);
-				p.set(m-1,n,i-1,j);
+		if(p.m>=2){
+			if(1<=p.i){
+				maxSucc = f_accel(p.m-1,p.n,p.i-1,p.j);
+				bestPlay = new Plateau(p.m-1,p.n,p.i-1,p.j);
 			}
 			else {
-				maxSucc = f_accel(1,n,i,j);
-				p.set(1,n,i,j);
+				maxSucc = f_accel(1,p.n,p.i,p.j);
+				bestPlay = new Plateau(1,p.n,p.i,p.j);
 			}
 		}
 		else {
-			if(1<=j) {
-				maxSucc = f_accel(m,n-1,i,j-1);
-				p.set(m,n-1,i,j-1);
+			if(1<=p.j) {
+				maxSucc = f_accel(p.m,p.n-1,p.i,p.j-1);
+				bestPlay = new Plateau(p.m,p.n-1,p.i,p.j-1);
 			}
 			else {
-				maxSucc = f_accel(m,1,i,j);
-				p.set(m,1,i,j);
+				maxSucc = f_accel(p.m,1,p.i,p.j);
+				bestPlay = new Plateau(p.m,1,p.i,p.j);
 			}
 		}
-		int maxSuccNegatif=42;
-		boolean allSuccStrictPositif = true;
 		int temp;
-		if(m>=2){
-			for(int k=1;k<m;k++) {
-				if(k<=i) {
-					temp = f_accel(m-k,n,i-k,j);
-					p.set(m-k,n,i-k,j);
+		Plateau tempPlateau;
+		if(p.m>=2){
+			for(int k=1;k<p.m;k++) {
+				if(k<=p.i) {
+					temp = f_accel(p.m-k,p.n,p.i-k,p.j);
+					tempPlateau.set(p.m-k,p.n,p.i-k,p.j);
 				}
 				else {
-					temp = f_accel(k,n,i,j);
-					p.set(m-k,n,i-k,j);
+					temp = f_accel(k,p.n,p.i,p.j);
+					tempPlateau.set(k,p.n,p.i,p.j);
 				}
-				maxSucc = Math.max(temp, maxSucc);
-				if(temp<=0 && allSuccStrictPositif) {
-					allSuccStrictPositif = false;
-					maxSuccNegatif = temp;
-				}
-				if(!allSuccStrictPositif) {
-					if(temp <= 0) {
-						maxSuccNegatif = Math.max(temp,maxSuccNegatif);
-					}
+				if(temp > maxSucc) {
+					maxSucc = temp;
+					bestPlay.copy(tempPlateau);
 				}
 			}
 		}
-		if(n>=2){
-			for(int k=1;k<n;k++) {
-				if(k<=j) {
-					temp = f_accel(m,n-k,i,j-k);
-					p.set(m,n-k,i,j-k);
+		if(p.n>=2){
+			for(int k=1;k<p.n;k++) {
+				if(k<=p.j) {
+					temp = f_accel(p.m,p.n-k,p.i,p.j-k);
+					tempPlateau.set(p.m,p.n-k,p.i,p.j-k);
 				}
 				else {
-					temp = f_accel(m,k,i,j);
-					p.set(m,k,i,j);
+					temp = f_accel(p.m,k,p.i,p.j);
+					tempPlateau.set(p.m,k,p.i,p.j);
 				}
-				maxSucc = Math.max(temp, maxSucc);
-				if(temp<=0 && allSuccStrictPositif) {
-					allSuccStrictPositif = false;
-					maxSuccNegatif = temp;
-				}
-				if(!allSuccStrictPositif) {
-					if(temp <= 0) {
-						maxSuccNegatif = Math.max(temp,maxSuccNegatif);
-					}
+				if(temp > maxSucc) {
+					maxSucc = temp;
+					bestPlay.copy(tempPlateau);
 				}
 			}
 		}
-		int result;
-		if(allSuccStrictPositif) {
-			result = -(1+maxSucc);
-		}
-		else {
-			result = 1 - maxSuccNegatif;
-		}
-		cacheWriteAccel(m,n,i,j,result);
-		return result;
+		p.copy(bestPlay);
+		return maxSucc;
 	}
 	
 }
